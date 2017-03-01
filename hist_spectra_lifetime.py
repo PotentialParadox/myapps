58226570c462a83b3cd7da29438e09c450e8ada7
Dustin Tracy <dtracy.uf@gmail.com>
2017-02-28 17:29:23 -0700

    renamed and commented for clarity

--- hist_spectra_lifetime.py
+++ hist_spectra_lifetime.py
@@ -2,7 +2,7 @@ import numpy as np
 from math import exp
 
 # Change here to the number of excited states propagated
-number_states = 5
+number_states = 1
 # Change here if you want more points per gaussian ?
 number_gauss = 100
 # Change the parameter for full width at half max in eV
@@ -14,20 +14,33 @@ x_minimum = 2.0
 # Change here the maximum energy in (eV)
 x_maximum = 4.1
 # Change here the input file
-file_input = 'spectra.input'
+file_input = 'spectra_flu.input'
 # Change here the output file
-file_output = 'spectra.output'
+file_output = 'spectra_flu.output'
 
 
-def get_fwhm_energy_value(energy, fwhm, gauss_index, n_gauss):
+def get_gaus_point_energy_value(energy, fwhm, gauss_index, n_gauss):
+    # The real energy value will be the center value.
+    # The outer indexes will be the real energy + or - 2 * fwhm.
+    # The possible return array thus looks like
+    # | E@-2fwhm | .. | E | .. | E@2fwhm |
+    # Energy is the energy from the input
+    # fwhm is the full width at half max given as a parameter
+    # Gauss index is the current index we're returning
+    # n_gauss is the number of total indexes
     return energy - (2.0 * fwhm) + ((gauss_index+1) * 4.0 * fwhm / n_gauss)
 
 
-def calculate_exponential(energy, strength, sigma, x_value_of_half_width):
+def calculate_exponential(energy_max, strength, sigma, gaus_point_energy):
+    # Strength is the oscillator strength from the input file
+    # The energy max is the original energy from the input file
+    # Sigma is the sigma that was defined previously in the program
+    # Gaus_point energy is the value of the energy that we extrapolated
+    # from the the original energy and an assumed normalized curve
     w = strength
-    u = energy
+    u = energy_max
     s = sigma
-    x = x_value_of_half_width
+    x = gaus_point_energy
     return (w / u**2) * exp((-(x - u)**2) / (2 * s**2))
 
 
@@ -56,12 +69,19 @@ def calculate_spectra(n_states, n_gauss, fwhm, n_bins, x_min, x_max, file_in, fi
         for state in range(n_states):
             energy = data[time_step][2*state]
             strength = data[time_step][2*state+1]
+            # We will take each energy point and spread its corresponding strength across adjacent energy values
+            # based on an assumed normal distribution.
             for gauss_point in range(n_gauss):
-                energy_at_lower_fwhm = get_fwhm_energy_value(energy, fwhm, gauss_point, n_gauss)
-                normalized_strength = calculate_exponential(energy, strength, sigma, energy_at_lower_fwhm)
-                x_index = int(energy_at_lower_fwhm/divisor) + 1
-                strengths[state, x_index] += normalized_strength
-                total_strength += normalized_strength
+                # First get the energy value within our range from -2fwhm to +2fwhm
+                gaus_point_energy = get_gaus_point_energy_value(energy, fwhm, gauss_point, n_gauss)
+                # Second turn that energy point into an index that we can use for our histogram
+                x_index = int(gaus_point_energy/divisor) + 1
+                # Third calculate how much strength will be appropriate for this index
+                appropriated_strength = calculate_exponential(energy, strength, sigma, gaus_point_energy)
+                # Add to index
+                strengths[state, x_index] += appropriated_strength
+                # Keep track of total oscillator strength
+                total_strength += appropriated_strength
 
     fo = open(file_out, 'w')
     for x_index in range(int(x_min/divisor), int(x_max/divisor)):
