95f723bbda4d1741a7cd04d35d01f6fcec8058fa
Dustin Tracy <dtracy.uf@gmail.com>
2017-08-31 16:09:07 -0400

    upgraded to argparse

--- find_spectra_shift.py
+++ find_spectra_shift.py
@@ -4,15 +4,19 @@ spectra given the filename of the
 file containing both spectra
 and the units you prefer it to be in
 '''
-import sys
+import argparse
 import find_shift
 import numpy as np
 
-FILENAME1 = sys.argv[1]
-FILENAME2 = sys.argv[2]
-UNIT = sys.argv[3]
-DATA1 = np.loadtxt(FILENAME1)
-DATA2 = np.loadtxt(FILENAME2)
+parser = argparse.ArgumentParser()
+parser.add_argument("--file1", help="The first file you want read the first state from")
+parser.add_argument("--file2", help="The first file you want read the first state from")
+parser.add_argument("--unit", help="0-ev, 1-nm", type=int, default=0)
+args = parser.parse_args()
+
+UNIT = "ev" if args.unit == 0 else "nm"
+DATA1 = np.loadtxt(args.file1)
+DATA2 = np.loadtxt(args.file2)
 DATA = DATA1
 DATA[:, 3] = DATA2[:, 3]
-print(find_shift.find_shift(DATA, UNIT))
+print(find_shift.find_shift(DATA, UNIT), UNIT)
