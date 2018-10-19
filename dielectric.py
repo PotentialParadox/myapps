d8f6a21264271cea3bad7956d72976ac13a468cb
Dustin Tracy <dtracy.uf@gmail.com>
2018-10-19 13:01:50 -0400

    Fixed dielectric algorithm

--- dielectric.py
+++ dielectric.py
@@ -11,13 +11,19 @@ import numpy as np
 import argparse
 import operator
 from my_math import quadratic_formula
-from my_constants import E0, KB, AM_TO_COULOMBMETER, ANGSTROM_TO_METER
+from my_constants import E0, KB, AE_TO_COULOMBMETER, ANGSTROM_TO_METER, AE_TO_DEBYE
 
 def product(xs):
     return reduce(operator.mul, xs, 1)
 
 def a_to_m(x):
     return x * 1E-10
+
+def average_dipole(dipoles):
+    return np.average(dipoles, axis=0)
+
+def dipole_magnitudes(dipoles):
+    return np.array([np.linalg.norm(x) for x in dipoles])
 
 def get_dipoles(parmtop, trajectory_file):
     command = bytes("parm {}\n"\
@@ -28,24 +34,23 @@ def get_dipoles(parmtop, trajectory_file):
     proc = Popen(["cpptraj"], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
     cpptraj_stdout = proc.communicate(input=command)[0]
     lines = open('dipole.out','r').readlines()
-    dipole_file = "".join(lines[1:])
+    dipole_file = "".join(lines[1::100])
     data = np.fromstring(dipole_file, sep=" ")
     data = np.reshape(data, (-1,7))
     return data[:,1:4]
 
-def convert_to_totals(xs):
-    return [np.linalg.norm(x)*AM_TO_COULOMBMETER for x in xs]
+def convert_to_debye(xs):
+    return [[y*AE_TO_COULOMBMETER for y in x] for x in xs]
 
 def calculate_dielectric(dipoles, V_meters, T_kelvon):
+    '''Estimates the dielectric of a box of solvent using "On the accurate
+    calculation of the dielectric constant and the diffusion coefficient from
+    molecular dynamics simulations: the case of SPC/E water Units are (debye,
+    m^3, K)
+    [dx,dy,dz] -> v -> t -> io ()
     '''
-    Estimates the dielectric of a box of solvent using
-    equation 2 of "Simple liquid models with corrected dielectric constants"
-    by Christopher J. Fennell
-    Units are (debye, m^3, K)
-    '''
-    s_exp_dipoles = np.power(np.average(dipoles),2)
-    dipoles_2 = np.power(dipoles, 2)
-    expval_dipoles_2 = np.average(dipoles_2)
+    s_exp_dipoles = np.dot(average_dipole(dipoles),average_dipole(dipoles))
+    expval_dipoles_2 = np.power(np.average(dipole_magnitudes(dipoles)),2)
     possible_answer = 1 + (expval_dipoles_2 - s_exp_dipoles) / (3.0 * E0 * V_meters * KB * T_kelvon)
     print(possible_answer)
 
@@ -55,7 +60,7 @@ parser.add_argument("-x", "--trajfile", help="Amber's trajectory file", default=
 parser.add_argument("-p", "--prmtop", help="Amber's top file", default="m1.prmtop")
 args = parser.parse_args()
 
-dipoles = convert_to_totals(get_dipoles(args.prmtop, args.trajfile)) # Debye
+DIPOLES = convert_to_debye(get_dipoles(args.prmtop, args.trajfile)) # Debye
 volume = product([a_to_m(x) for x in find_box(open('nasqm_ground.out','r'))]) # m^3
 temperature=300 # Kelvin
-calculate_dielectric(dipoles, volume, temperature)
+d = [calculate_dielectric(DIPOLES[:x], volume, temperature) for x in range(1,len(DIPOLES))]
