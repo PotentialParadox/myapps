626e9460f10f7ac1779ea94031184839e489eb34
Dustin Tracy <dtracy.uf@gmail.com>
2018-11-14 15:11:19 -0500

    updated dipole.py to use pytraj and multiple trajs

--- dipole.py
+++ dipole.py
@@ -1,76 +1,41 @@
 '''
-Calculates the dipoles of an md simultions using a file containing charges
+Calculates the dipoles of a md simultion set using a file containing charges
 dustin tracy (dtrac.uf@gmail.com)
 '''
 import argparse
-import re
+import pytraj as pt
 import numpy as np
-from my_constants import AMBERCHARGE_TO_FRANKLIN, ANGSTROM_TO_CM, FRANKLINCM_TO_DEBYE
+from my_constants import AE_TO_DEBYE
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--parmtop_file", "-p", help="Parmtop FILE")
-    parser.add_argument("--restart_file", "-r", help="Non-binary restart file")
+    parser.add_argument("n_trajs", help="number of trajectories", type=int)
+    parser.add_argument("traj_time", help="time of each trajectory", type=float)
+    parser.add_argument("--flu", help="apply to fluorescence", action="store_true")
+    parser.add_argument("--full", help="print entire dipole list", action="store_true")
+    parser.add_argument("--mags", help="print list of dipole magnitudes", action="store_true")
+    parser.add_argument("--average", help="print average dipole", action="store_true")
     args = parser.parse_args()
+    suffix = 'flu' if args.flu else 'abs'
+    dipoless = get_dipoles(args.n_trajs, suffix)
+    if args.average:
+        print(average_dipole(dipoless))
 
-    charges = find_charges(args.parmtop_file)
-    coordinates = find_coordinates(args.restart_file)
-    calculate_dipole(charges, coordinates)
+def average_dipole(dipoles):
+    print(diMags(dipoles))
+    return np.average(diMags(dipoles))
 
-def find_charges(parmtop_file):
-    p_charges = re.compile("CHARGE")
-    p_flags = re.compile("FLAG")
-    charge_block = ""
-    with open(parmtop_file, 'r') as filein:
-        for line in filein:
-            if re.search(p_charges, line):
-                filein.readline()
-                for morelines in filein:
-                    if re.search(p_flags, morelines):
-                        break
-                    charge_block += morelines + "\n"
-    charge_list = [float(x) for x in charge_block.split()]
-    return np.array(charge_list)
+def diMags(dipoless):
+    return np.array([[np.linalg.norm(x) for x in dipoles] for dipoles in dipoless])
 
-def find_coordinates(restart_file):
-    coordinate_block = ""
-    number_atoms = None
-    with open(restart_file, 'r') as filein:
-        filein.readline()
-        second_line = filein.readline()
-        number_atoms = int(second_line.split()[0])
-        coordinate_block = ""
-        for line in filein:
-            coordinate_block += line + "\n"
-    n_coords = number_atoms * 3
-    coord_list = [float(x) for x in coordinate_block.split()[:n_coords]]
-    return reshape_coordinates(coord_list)
+def get_dipoles(nTrajs, suffix):
+    return convert_to_debye(np.array([get_dipole(traj, suffix) for traj in range(1, nTrajs+1)]))
 
-def reshape_coordinates(coordinates):
-    n_coords = int(len(coordinates) / 3)
-    coords = []
-    for coord in range(n_coords):
-        atom_coord = []
-        for dim in range(3):
-            index = 3*coord + dim
-            atom_coord.append(coordinates[index])
-        coords.append(atom_coord)
-    return np.array(coords)
+def get_dipole(traj, suffix):
+    traj = pt.load('{}/nasqm_{}_{}.nc'.format(traj, suffix, traj), top='m1.prmtop')
+    return pt.analysis.vector.dipole(traj)
 
-def calculate_dipole(amber_charges, coordinates):
-    '''
-    Calculate the dipole of a file with formated as amber_charge charge, X, Y, Z
-    '''
-    charges = amber_charges * AMBERCHARGE_TO_FRANKLIN
-    coords = coordinates * ANGSTROM_TO_CM
-    dipoles = np.dot(charges[:, None].T, coords) * FRANKLINCM_TO_DEBYE
-    total = np.linalg.norm(dipoles)
-
-    answer_string = "Dipole: X= {: 10.4f}, Y= {: 10.4f}, Z= {: 10.4f}"\
-                                " Total= {: 10.4f}".format(dipoles[0][0], dipoles[0][1],
-                                                           dipoles[0][2], total)
-
-    print(answer_string)
-
+def convert_to_debye(xss):
+    return np.array([[x*AE_TO_DEBYE for x in xs] for xs in xss])
 
 main()
