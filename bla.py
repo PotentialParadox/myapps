f5f82cb6bfaa2748fd13102201da41e1e85f7c45
Dustin Tracy <dtracy.uf@gmail.com>
2018-12-02 12:22:42 -0500

    Plot each distance involved in the bla calculations

--- bla.py
+++ bla.py
@@ -12,9 +12,9 @@ def main():
     args = parser.parse_args()
     suffix = 'flu' if args.flu else 'abs'
     if args.plot:
-        blas = np.load("bla_{}.npy".format(suffix))
-        blas = np.average(blas, axis=0)
-        plotter(blas, suffix, args.traj_time)
+        dss = np.load("bla_{}.npy".format(suffix))
+        dss = np.average(dss, axis=1)
+        plotter(dss, suffix, args.traj_time)
     else:
         d1 = getDistances(args.n_trajs, suffix, 6, 7)
         d2 = getDistances(args.n_trajs, suffix, 7, 8)
@@ -29,14 +29,31 @@ def getDistance(traj, suffix, atom1, atom2):
 def getDistances(nTrajs, suffix, atom1, atom2):
     return [getDistance(traj, suffix, atom1, atom2) for traj in range(1, nTrajs+1)]
 
-def plotter(bla, suffix, time):
+def plotter(dss, suffix, time):
+    d1s = dss[0]
+    d2s = dss[1]
+    d3s = dss[2]
+    d1 = np.average(d1s[-100:])
+    d2 = np.average(d2s[-100:])
+    d3 = np.average(d3s[-100:])
+    print("d1: ", d1)
+    print("d2: ", d2)
+    print("d3: ", d3)
+    print("d1+d2/2", (d1+d3)/2)
+    print("bla", (d1+d3)/2 - d2)
+    t = np.linspace(0, time, len(d1s), endpoint=True)
+    plt.plot(t, d1s)
+    plt.plot(t, d2s)
+    plt.plot(t, d3s)
+    plt.show()
+    bla = np.subtract(np.true_divide(np.add(d1s, d3s), 2), d2s)
     t = np.linspace(0, time, len(bla), endpoint=True)
     plt.plot(t, bla)
     if suffix == 'abs':
         suffix = 'S0'
     else:
         suffix = 'S1'
-    plt.title("BLA {}".format(suffix))
+    plt.title("vac BLA {}".format(suffix))
     plt.xlabel("time ps")
     plt.savefig("{}_bla".format(suffix))
     plt.show()
