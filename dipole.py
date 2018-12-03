d804e724c8021106784c4efa0d67949f107ef622
Dustin Tracy <dtracy.uf@gmail.com>
2018-12-03 11:18:09 -0500

    saving dipoles

--- dipole.py
+++ dipole.py
@@ -5,6 +5,7 @@ dustin tracy (dtrac.uf@gmail.com)
 import argparse
 import pytraj as pt
 import numpy as np
+import matplotlib.pyplot as plt
 from my_constants import AE_TO_DEBYE
 
 def main():
@@ -15,16 +16,25 @@ def main():
     parser.add_argument("--full", help="print entire dipole list", action="store_true")
     parser.add_argument("--mags", help="print list of dipole magnitudes", action="store_true")
     parser.add_argument("--average", help="print average dipole", action="store_true")
+    parser.add_argument("--plot", help="plot dipole vs time", action="store_true")
     args = parser.parse_args()
     suffix = 'flu' if args.flu else 'abs'
     dipoless = get_dipoles(args.n_trajs, suffix)
     if args.average:
         print(average_dipole(dipoless))
     if args.mags:
-        print_mags(dipoless)
+        print_mags(dipoless, suffix)
+    if args.plot:
+        plot_dipoles(suffix, args.traj_time)
 
-def print_mags(dipoless):
-    print(np.average(diMags(dipoless), axis=0))
+def plot_dipoles(suffix, traj_time):
+    dips = np.load("dipoles_{}.npy".format(suffix))
+    t = np.linspace(0, traj_time, len(dips), endpoint=True)
+    plt.plot(t, dips)
+    plt.show()
+
+def print_mags(dipoless, suffix):
+    np.save("dipoles_{}.npy".format(suffix), np.average(diMags(dipoless), axis=0))
 
 def average_dipole(dipoless):
     return np.average(diMags(dipoless))
