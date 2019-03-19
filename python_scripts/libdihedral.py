3673075c5da321876b51dd29dc3791b3d2d853b5
Dustin Tracy <dtracy.uf@gmail.com>
2019-03-19 05:44:31 -0400

    dihedral works with new hierarchy

--- python_scripts/libdihedral.py
+++ python_scripts/libdihedral.py
@@ -4,7 +4,7 @@ from functools import reduce
 import matplotlib.pyplot as plt
 
 def getDihedral(suffix, traj, atomss):
-    traj = pt.load('{}/nasqm_{}_{}.nc'.format(traj, suffix, traj), top='m1.prmtop')
+    traj = pt.load('{0}/traj_{1}/nasqm_{0}_{1}.nc'.format(suffix, traj), top='m1.prmtop')
     return [dihedralAbs(pt.dihedral(traj, '@{} @{} @{} @{}'.format(atoms[0],
                                                                    atoms[1],
                                                                    atoms[2],
@@ -20,11 +20,15 @@ def dihedralAbs(dihs):
     return [min(abs(di), 180-abs(di)) for di in dihs]
 
 def plotter(dihs, suffix, time, solvent):
-    t = np.linspace(0, time, len(dihs), endpoint=True)
-    plt.plot(t, dihs)
-    suffix = 'S0' if suffix == 'abs' else 'S1'
-    plt.title("{} Dihedral {}".format(solvent, suffix))
-    plt.xlabel("time ps")
+    fig1, ax1 = plt.subplots()
+    plot_dihedral(ax1, dihs, time, suffix, solvent)
     plt.savefig("{}_dihedral".format(suffix))
     plt.show()
     print("Average Dihedral: {} Degrees".format(np.average(dihs)))
+
+def plot_dihedral(ax, dihs, time, suffix, solvent):
+    t = np.linspace(0, time, len(dihs), endpoint=True)
+    ax.plot(t, dihs)
+    suffix = 'S0' if suffix == 'abs' else 'S1'
+    ax.set_title("Dihedral at {} in {}".format(suffix, solvent))
+    ax.set_xlabel("time ps")
