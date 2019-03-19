8e95db137f636ed2850ba5bc782673628d6a8ebc
Dustin Tracy <dtracy.uf@gmail.com>
2019-03-19 05:43:45 -0400

    updated bla.py to produce fourier transform

--- python_scripts/libbla.py
+++ python_scripts/libbla.py
@@ -2,6 +2,7 @@ import pytraj as pt
 import numpy as np
 import os.path
 import matplotlib.pyplot as plt
+import seaborn as sns
 from python_scripts.libmyconstants import HZ_TO_WAVENUMBER
 from python_scripts.libmymath import myFourierTransform
 
@@ -29,6 +30,60 @@ def plotter(dss, suffix, time, solvent):
     d1s = np.average(dss[0], axis=0)
     d2s = np.average(dss[1], axis=0)
     d3s = np.average(dss[2], axis=0)
+    timestep = time / len(d1s) * 1e-12
+
+    print_averages(d1s, d2s, d3s, timestep)
+
+    sns.set()
+    sns.set_style("white")
+    sns.set_style("ticks")
+
+    fig1, ax1 = plt.subplots()
+    plot_fourier(ax1, timestep, d1s)
+
+    fig2, ax2 = plt.subplots()
+    plot_individuals(ax2, time, d1s, d2s, d3s)
+
+    fig3, ax3 = plt.subplots()
+    plot_bla(ax3, time, d1s, d2s, d3s, suffix, solvent)
+
+    plt.show()
+
+def plot_bla(ax, time, d1s, d2s, d3s, suffix, solvent):
+    bla = np.subtract(np.true_divide(np.add(d1s, d3s), 2), d2s)
+    t = np.linspace(0, time, len(bla), endpoint=True)
+    ax.plot(t, bla)
+    if suffix == 'abs':
+        suffix = 'S0'
+    else:
+        suffix = 'S1'
+    ax.set_title("{} BLA {}".format(solvent, suffix))
+    ax.set_xlabel("Time (ps)")
+    ax.set_ylabel(r"Deformation ($\AA$)")
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.tick_params(direction='in')
+    print("Average Bla: {}A".format(np.average(bla)))
+
+def plot_fourier(ax, ts, yi):
+    x, y = myFourierTransform(yi, ts)
+    x = x * HZ_TO_WAVENUMBER
+    ax.axes.get_yaxis().set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.set_xlabel("cm$^{-1}$")
+    ax.tick_params(direction='in')
+    x, y = truncate_to(5000, x, y)
+    ax.plot(x[1:], y[1:])
+
+def truncate_to(limit, xs, ys):
+    filtered = [(x[0], x[1]) for x in zip(xs, ys) if x[0] <= limit]
+    new_xs = [x[0] for x in filtered]
+    new_ys = [x[1] for x in filtered]
+    return new_xs, new_ys
+
+def print_averages(d1s, d2s, d3s, timestep):
     d1 = np.average(d1s[-100:])
     d2 = np.average(d2s[-100:])
     d3 = np.average(d3s[-100:])
@@ -38,39 +93,17 @@ def plotter(dss, suffix, time, solvent):
     print("d1+d2/2", (d1+d3)/2)
     print("bla", (d1+d3)/2 - d2)
     print("nsteps", len(d1s))
-    timestep = time / len(d1s) * 10e-6
     print("timestep", timestep)
     print("steps", len(d1s))
-    # Testing
-    # ts = 0.001
-    # N = 1000
-    # xs = np.linspace(0,N*ts,N)
-    # ys = np.sin(50.0 * 2.0*np.pi*xs) + 0.5*np.sin(80.0 * 2.0*np.pi*xs)
-    # End of Testing
-    ys = d1s
-    ts = timestep
-    x, y = myFourierTransform(ys, ts)
-    x = x * HZ_TO_WAVENUMBER
-    plt.plot(x[1:], y[1:])
-    plt.show()
-    # Fourier Testing
+
+def plot_individuals(ax, time, d1s, d2s, d3s):
     t = np.linspace(0, time, len(d1s), endpoint=True)
-    plt.plot(t, d1s)
-    plt.plot(t, d2s)
-    plt.plot(t, d3s)
-    plt.xlabel("Time (ps)")
-    plt.ylabel("Bond Length (A)")
-    plt.show()
-    bla = np.subtract(np.true_divide(np.add(d1s, d3s), 2), d2s)
-    t = np.linspace(0, time, len(bla), endpoint=True)
-    plt.plot(t, bla)
-    if suffix == 'abs':
-        suffix = 'S0'
-    else:
-        suffix = 'S1'
-    plt.title("{} BLA {}".format(solvent, suffix))
-    plt.xlabel("Time (ps)")
-    plt.ylabel("Deformation (A)")
-    plt.savefig("{}_bla".format(suffix))
-    plt.show()
-    print("Average Bla: {}A".format(np.average(bla)))
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.tick_params(direction='in')
+    ax.plot(t, d1s, label='d1')
+    ax.plot(t, d2s, label='d2')
+    ax.plot(t, d3s, label='d3')
+    ax.legend(frameon=False, bbox_to_anchor=(1, 1.1))
+    ax.set_xlabel("Time (ps)")
+    ax.set_ylabel(r"Bond Length ($\AA$)")
